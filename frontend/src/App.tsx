bac89900d01f1f57396de11b100718b981b16fe9
Nguyen Nguyen <nasongnguyen@gmail.com>
2025-05-28 13:52:46 +0300

    cors config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
+import EndpointTesting from './components/tests/EndpointTesting';
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <EndpointTesting />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />

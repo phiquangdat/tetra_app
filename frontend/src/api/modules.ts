50a7035c553f64b4e9657c48216e25fd676d5eb3
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-05-30 16:15:36 +0300

    Create initial ModulePage component and fetchModuleById API function

---
+++ frontend/src/api/modules.ts
@@ -0,0 +1,19 @@
+export interface Module {
+    id: string;
+    title: string;
+    topic: string;
+    description: string;
+    points: number;
+    coverUrl: string
+}
+
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
+export async function fetchModuleById(id: string): Promise<Module> {
+    const response = await fetch(`${BASE_URL}/api/modules/${id}`);
+    if (!response.ok) {
+        throw new Error('Failed to fetch module');
+    }
+    const data: Module = await response.json();
+    return data;
+}

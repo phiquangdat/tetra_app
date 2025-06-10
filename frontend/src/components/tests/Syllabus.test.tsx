b474f03df64574ed1a5329e6f340f15b2b84a02e
Nguyen Nguyen <nasongnguyen@gmail.com>
2025-06-10 15:44:35 +0300

    Unit test for Syllabus component

--- frontend/src/components/tests/Syllabus.test.tsx
+++ frontend/src/components/tests/Syllabus.test.tsx
@@ -1,54 +1,44 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Syllabus from '../Syllabus';
-import { GetModules, GetUnitTitleByModuleId } from '../../api/http';
+import { GetUnitTitleByModuleId } from '../../api/http';
 import { beforeEach, describe, expect, it, vi, type Mock } from 'vitest';
 
-const mockModules = [{ id: 'module1' }, { id: 'module2' }];
-const mockUnitTitles: Record<string, string> = {
-  module1: 'Data Protection',
-  module2: 'Cybersecurity Fundamentals',
-};
+const mockModuleId = 'aaeacc19-4619-4f0a-8249-88ce37cf2a50';
+const mockUnitTitles = [
+  { moduleId: mockModuleId, title: 'Cybersecurity Essentials' },
+];
 
 vi.mock('../../api/http', () => ({
-  GetModules: vi.fn(),
   GetUnitTitleByModuleId: vi.fn(),
 }));
 
 describe('Syllabus Component', () => {
   beforeEach(() => {
     vi.resetAllMocks();
+  });
 
-    (GetModules as Mock).mockResolvedValue(mockModules);
-    (GetUnitTitleByModuleId as Mock).mockImplementation((id: string) =>
-      Promise.resolve(mockUnitTitles[id]),
+  const renderSyllabus = () => render(<Syllabus moduleID={mockModuleId} />);
+
+  it('renders unit titles after successful data fetch', async () => {
+    (GetUnitTitleByModuleId as Mock).mockResolvedValueOnce(mockUnitTitles);
+
+    renderSyllabus();
+
+    await waitFor(() =>
+      expect(
+        screen.getByText('Unit 1: Cybersecurity Essentials'),
+      ).toBeInTheDocument(),
     );
   });
 
-  const renderSyllabus = () => render(<Syllabus />);
+  it('displays error message when moduleID is not valid', async () => {
+    render(<Syllabus moduleID={null} />);
 
-  it('renders the correct unit titles after data is fetched', async () => {
-    renderSyllabus();
-
-    await waitFor(() => {
-      expect(screen.getByText('Unit 1: Data Protection')).toBeInTheDocument();
+    await waitFor(() =>
       expect(
-        screen.getByText('Unit 2: Cybersecurity Fundamentals'),
-      ).toBeInTheDocument();
-    });
-  });
-
-  it('should render "moduleId query parameter is required" on fetch /api/units', async () => {
-    const mockFetch = vi.fn().mockRejectedValueOnce({
-      response: {
-        data: 'moduleId query parameter is required',
-      },
-    });
-
-    try {
-      await mockFetch(`${import.meta.env.VITE_BACKEND_URL}/api/units`);
-    } catch (error) {
-      expect(error.response.data).toBe('moduleId query parameter is required');
-    }
+        screen.getByText('failed to fetch units: units is not found'),
+      ).toBeInTheDocument(),
+    );
   });
 });

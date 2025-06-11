ec3c2301a28c3e0e569717a19d0cf98b59e274db
Nguyen Nguyen <nasongnguyen@gmail.com>
2025-06-11 12:02:59 +0300

    add Route for UnitPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,18 @@ import { Route, Routes, useParams } from 'react-router-dom';
 
 import ModuleCards from './components/ModuleCards';
 import ModulePage from './components/ModulePage';
+import UnitPage from './components/UnitPage';
 
 function ModulePageWrapper() {
   const { id } = useParams();
   if (!id) return <div>Module ID not found</div>;
   return <ModulePage id={id} />;
+}
+
+function UnitPageWrapper() {
+  const { id } = useParams();
+  if (!id) return <div>Unit ID not found</div>;
+  return <UnitPage id={id} />;
 }
 
 function App() {
@@ -15,6 +22,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ModuleCards />} />
         <Route path="/modules/:id" element={<ModulePageWrapper />} />
+        <Route path="/unit/:id" element={<UnitPageWrapper />} />
       </Routes>
     </>
   );

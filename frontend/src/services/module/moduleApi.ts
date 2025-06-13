f02fb6f595995dba4708e30913acbab57bd00c91
phiquangdat <datphi020603@gmail.com>
2025-06-13 14:36:32 +0300

    Move API functions into /services folder

---
+++ frontend/src/services/module/moduleApi.ts
@@ -0,0 +1,37 @@
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
+export interface Module {
+  id: string;
+  title: string;
+  topic: string;
+  description: string;
+  points: number;
+  coverUrl: string;
+}
+
+export async function fetchModuleById(id: string): Promise<Module> {
+  const response = await fetch(`${BASE_URL}/api/modules/${id}`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch module');
+  }
+  const data: Module = await response.json();
+  return data;
+}
+
+export async function GetModules(): Promise<any> {
+  try {
+    const response = await fetch(`${BASE_URL}/api/modules`);
+    const modulesData = await response.json();
+
+    if (!response.ok) throw new Error('Failed to fetch modules');
+
+    return modulesData;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('Error fetching modules:', error.message);
+    } else {
+      console.error('Unknown error fetching modules', error);
+    }
+    return [];
+  }
+}

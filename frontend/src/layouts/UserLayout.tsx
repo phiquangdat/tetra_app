c6847029300be70b01781766dd64b817862dab37
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-06-19 15:00:41 +0300

    Add UnitContent context

--- frontend/src/layouts/UserLayout.tsx
+++ frontend/src/layouts/UserLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 import { QuizModalProvider } from '../context/QuizModalContext.tsx';
 import { UnitContentProvider } from '../context/UnitContentContext';
 import QuizStartModal from '../components/user/quiz/QuizStartModal.tsx';
+import { UnitContentProvider } from '../context/UnitContentContext';
 
 export default function UserLayout() {
   return (

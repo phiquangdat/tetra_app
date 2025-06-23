a8d6cf69579620531c89e75a34de90b942123ce8
AriTeil-Yevhenii-Nazarov <jevgenij.matvijovich.nazarov@gmail.com>
2025-06-23 11:54:22 +0000

    Edit App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,5 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { appRoutes } from './routes';
-
-
 const router = createBrowserRouter(appRoutes);
 
 export default function App() {

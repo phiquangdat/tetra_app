165b553725994c29875d5e0db296b33d30dfd901
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-06-23 16:59:29 +0300

    Add QuizContext to UserLayout to support quiz state

--- frontend/src/layouts/UserLayout.tsx
+++ frontend/src/layouts/UserLayout.tsx
@@ -3,15 +3,18 @@ import { QuizModalProvider } from '../context/QuizModalContext.tsx';
 import { UnitContentProvider } from '../context/UnitContentContext';
 import QuizStartModal from '../components/user/quiz/QuizStartModal.tsx';
 import { UnitContentProvider } from '../context/UnitContentContext';
+import { QuizProvider } from '../context/QuizContext.tsx';
 
 export default function UserLayout() {
   return (
     <QuizModalProvider>
       <UnitContentProvider>
-        <main>
-          <Outlet />
-          <QuizStartModal />
-        </main>
+        <QuizProvider>
+          <main>
+            <Outlet />
+            <QuizStartModal />
+          </main>
+        </QuizProvider>
       </UnitContentProvider>
     </QuizModalProvider>
   );

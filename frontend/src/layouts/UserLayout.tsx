08c99763dbe67072b0873d780956d9103a15d4fc
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-06-23 12:05:01 +0300

    Move quiz-related components into /user/quiz folder

--- frontend/src/layouts/UserLayout.tsx
+++ frontend/src/layouts/UserLayout.tsx
@@ -2,7 +2,6 @@ import { Outlet } from 'react-router-dom';
 import { QuizModalProvider } from '../context/QuizModalContext.tsx';
 import { UnitContentProvider } from '../context/UnitContentContext';
 import QuizStartModal from '../components/user/quiz/QuizStartModal.tsx';
-import { UnitContentProvider } from '../context/UnitContentContext';
 import { QuizProvider } from '../context/QuizContext.tsx';
 
 export default function UserLayout() {

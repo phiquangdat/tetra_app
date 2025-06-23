6ddbae6ae63f7d8ad43f044380aeb4fadfaf88ef
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-06-23 14:28:26 +0300

    refactor: move Home component and render Header inside it
    - Moved /home folder to /components/user/home
    - Moved Home.tsx into the new home folder
    - Updated Home.tsx to render Header directly
    - Removed Header rendering from App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { appRoutes } from './routes';
-import Header from './components/home/Header';
-
 const router = createBrowserRouter(appRoutes);
 
 export default function App() {
   return (
     <>
-      <Header />
       <RouterProvider router={router} />
     </>
   );

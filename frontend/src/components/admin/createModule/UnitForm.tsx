3713a08eb16bf70ebe65ff69511e1ab8d6f65a76
Nguyen Nguyen <nasongnguyen@gmail.com>
2025-06-24 10:27:45 +0300

    Implement "Add another unit" functionality in UnitForm

--- frontend/src/components/admin/createModule/UnitForm.tsx
+++ frontend/src/components/admin/createModule/UnitForm.tsx
@@ -1,20 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AddArticleModal from './AddArticleModal';
 import AddVideoModal from './AddVideoModal';
 
-function UnitForm() {
+type contentBlock = {
+  type: 'video' | 'article' | 'quiz';
+  data: any;
+};
+
+type Unit = {
+  unitNumber: number;
+  title?: string;
+  description?: string;
+  content: contentBlock[];
+};
+
+type UnitFormProps = {
+  unitNumber: number;
+  onChange: (updatedUnit: Unit) => void;
+};
+
+function UnitForm({ unitNumber = 1, onChange }: UnitFormProps) {
   const [isOpen, setIsOpen] = useState(true);
-  const [unitNumber, setUnitNumber] = useState(1);
   const [isArticleModalOpen, setIsArticleModalOpen] = useState(false);
   const [isVideoModalOpen, setIsVideoModalOpen] = useState(false);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [content, setContent] = useState<contentBlock[]>([]);
 
-  const incrementUnitNumber = () => {
-    setUnitNumber((prev) => prev + 1);
-  };
-
-  const handleAddUnit = () => {
-    incrementUnitNumber();
-  };
+  useEffect(() => {
+    onChange({
+      unitNumber,
+      title,
+      description,
+      content,
+    });
+  }, [unitNumber, title, description, content]);
 
   const handleContentBlockChange = (e: { target: { value: string } }) => {
     const value = e.target.value;
@@ -29,10 +49,20 @@ function UnitForm() {
 
   const handleArticleSave = () => {
     setIsArticleModalOpen(false);
+    const newContentBlock: contentBlock = {
+      type: 'article',
+      data: {},
+    };
+    setContent((prevContent) => [...prevContent, newContentBlock]);
   };
 
   const handleVideoSave = () => {
     setIsVideoModalOpen(false);
+    const newContentBlock: contentBlock = {
+      type: 'video',
+      data: {},
+    };
+    setContent((prevContent) => [...prevContent, newContentBlock]);
   };
 
   const icons = {
@@ -59,92 +89,80 @@ function UnitForm() {
       </svg>
     ),
   };
+
   return (
     <div>
-      <div>
-        <h2 className="text-2xl font-semibold text-gray-700 mb-6">Units</h2>
-        <form action="submit">
-          <div
-            className={`flex items-center justify-between px-3 py-4 mb-4 ${isOpen ? '' : 'bg-gray-100'} rounded-lg cursor-pointer transition-colors duration-200`}
-            onClick={() => setIsOpen(!isOpen)}
-          >
-            <h3 className="text-xl font-semibold text-gray-700">
-              Unit {unitNumber}
-            </h3>
-            {isOpen ? icons.chevronUp : icons.chevronDown}
-          </div>
-          {isOpen && (
-            <div className="space-y-6 max-w-110 mx-0 mb-11">
-              <div className="max-w-90 mb-11">
-                <label
-                  htmlFor="unitTitle"
-                  className="block mb-2 font-medium text-gray-700"
-                >
-                  Unit Title
-                </label>
-                <input
-                  type="text"
-                  id="unitTitle"
-                  name="unitTitle"
-                  required
-                  className="bg-white border-gray-400 border-2 w-full rounded-lg p-2 focus:outline-none focus:border-blue-500 transition-colors duration-200"
-                />
-              </div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div
+          className={`flex items-center justify-between px-3 py-4 mb-4 ${isOpen ? '' : 'bg-gray-100'} rounded-lg cursor-pointer transition-colors duration-200`}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <h3 className="text-xl font-semibold text-gray-700">
+            Unit {unitNumber}
+          </h3>
+          {isOpen ? icons.chevronUp : icons.chevronDown}
+        </div>
 
-              <div className="w-full mb-11">
-                <label
-                  htmlFor="unitDescription"
-                  className="block mb-2 font-medium text-gray-700"
-                >
-                  Unit Description
-                </label>
-                <textarea
-                  id="unitDescription"
-                  name="unitDescription"
-                  className="bg-white border-gray-400 border-2 w-full h-60 rounded-lg p-2 focus:outline-none focus:border-blue-500 transition-colors duration-200"
-                  style={{ resize: 'none' }}
-                  rows={4}
-                ></textarea>
-              </div>
-
-              <div className="w-48 mb-11">
-                <label
-                  htmlFor="contentBlocks"
-                  className="text-xl font-semibold"
-                >
-                  Content Blocks
-                </label>
-                <select
-                  id="contentBlocks"
-                  name="contentBlocks"
-                  onChange={handleContentBlockChange}
-                  className="bg-white border-gray-400 text-gray-700 border-2 w-full mt-6 rounded-lg p-2 focus:outline-none focus:border-blue-500 transition-colors duration-200"
-                >
-                  <option value="" disabled selected>
-                    + Add content
-                  </option>
-                  <option value="addVideo">Add video</option>
-                  <option value="addArticle">Add article</option>
-                  <option value="addQuiz">Add quiz</option>
-                </select>
-              </div>
+        {isOpen && (
+          <div className="space-y-6 max-w-110 mx-0 mb-11">
+            <div className="max-w-90 mb-11">
+              <label
+                htmlFor="unitTitle"
+                className="block mb-2 font-medium text-gray-700"
+              >
+                Unit Title
+              </label>
+              <input
+                type="text"
+                id="unitTitle"
+                name="unitTitle"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+                className="bg-white border-gray-400 border-2 w-full rounded-lg p-2 focus:outline-none focus:border-blue-500 transition-colors duration-200"
+              />
             </div>
-          )}
 
-          <div className="mx-auto my-6 flex justify-center">
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                handleAddUnit();
-              }}
-              className="bg-white border-gray-400 border-2 text-sm text-gray-700 px-2  rounded-lg cursor-pointer w-44 h-10 hover:bg-gray-100 transition-colors duration-200 flex items-center gap-2"
-            >
-              <span className="text-xl pb-1">+</span>
-              Add another unit
-            </button>
+            <div className="w-full mb-11">
+              <label
+                htmlFor="unitDescription"
+                className="block mb-2 font-medium text-gray-700"
+              >
+                Unit Description
+              </label>
+              <textarea
+                id="unitDescription"
+                name="unitDescription"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className="bg-white border-gray-400 border-2 w-full h-60 rounded-lg p-2 focus:outline-none focus:border-blue-500 transition-colors duration-200"
+                style={{ resize: 'none' }}
+                rows={4}
+              />
+            </div>
+
+            <div className="w-48 mb-11">
+              <label htmlFor="contentBlocks" className="text-xl font-semibold">
+                Content Blocks
+              </label>
+              <select
+                id="contentBlocks"
+                name="contentBlocks"
+                onChange={handleContentBlockChange}
+                className="bg-white border-gray-400 text-gray-700 border-2 w-full mt-6 rounded-lg p-2 focus:outline-none focus:border-blue-500 transition-colors duration-200"
+                defaultValue=""
+              >
+                <option value="" disabled>
+                  + Add content
+                </option>
+                <option value="addVideo">Add video</option>
+                <option value="addArticle">Add article</option>
+                <option value="addQuiz">Add quiz</option>
+              </select>
+            </div>
           </div>
-        </form>
-      </div>
+        )}
+      </form>
 
       <AddArticleModal
         isOpen={isArticleModalOpen}

ac012a4ba3d9dd76e36fdcafca83ccfa2568e10d
phiquangdat <datphi020603@gmail.com>
2025-07-25 17:02:20 +0300

    Wrap AuthProvider into AdminLayout and UserLayout

--- frontend/src/layouts/AdminLayout.tsx
+++ frontend/src/layouts/AdminLayout.tsx
@@ -6,6 +6,7 @@ import Footer from '../components/ui/Footer.tsx';
 import { ModuleContextProvider } from '../context/admin/ModuleContext.tsx';
 import { UnitContextProvider } from '../context/admin/UnitContext.tsx';
 import { ContentBlockContextProvider } from '../context/admin/ContentBlockContext.tsx';
+import { AuthProvider } from '../context/auth/AuthContext.tsx';
 
 const AdminLayout = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -13,38 +14,40 @@ const AdminLayout = () => {
   const toggleSidebar = () => setSidebarOpen((prev) => !prev);
 
   return (
-    <ModuleContextProvider>
-      <UnitContextProvider>
-        <ContentBlockContextProvider>
-          <div className="min-h-screen flex flex-col">
-            <SharedHeader
-              onHamburgerClick={toggleSidebar}
-              isSidebarOpen={sidebarOpen}
-            />
-            <div className="flex flex-1 relative">
-              <div
-                className={`
-            absolute z-50 h-full
-            transform transition-transform duration-300 ease-in-out
-            ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'}
-          `}
-              >
-                <AdminSidebar />
+    <AuthProvider>
+      <ModuleContextProvider>
+        <UnitContextProvider>
+          <ContentBlockContextProvider>
+            <div className="min-h-screen flex flex-col">
+              <SharedHeader
+                onHamburgerClick={toggleSidebar}
+                isSidebarOpen={sidebarOpen}
+              />
+              <div className="flex flex-1 relative">
+                <div
+                  className={`
+              absolute z-50 h-full
+              transform transition-transform duration-300 ease-in-out
+              ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'}
+            `}
+                >
+                  <AdminSidebar />
+                </div>
+                <main
+                  className={`
+              flex-1 transition-all duration-300 ease-in-out
+              ${sidebarOpen ? 'ml-64' : ''}
+            `}
+                >
+                  <Outlet />
+                </main>
               </div>
-              <main
-                className={`
-            flex-1 transition-all duration-300 ease-in-out
-            ${sidebarOpen ? 'ml-64' : ''}
-          `}
-              >
-                <Outlet />
-              </main>
+              <Footer />
             </div>
-            <Footer />
-          </div>
-        </ContentBlockContextProvider>
-      </UnitContextProvider>
-    </ModuleContextProvider>
+          </ContentBlockContextProvider>
+        </UnitContextProvider>
+      </ModuleContextProvider>
+    </AuthProvider>
   );
 };
 

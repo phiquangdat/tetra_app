23d2b96906c550d348574b3597b8f88b26254bb0
Nguyen Nguyen <nasongnguyen@gmail.com>
2025-07-27 19:22:13 +0300

    Swap “Login” button for “Logout” when user Is authenticated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { appRoutes } from './routes';
 const router = createBrowserRouter(appRoutes);
+import { AuthProvider } from './context/auth/AuthContext';
 
 export default function App() {
   return (
-    <>
+    <AuthProvider>
       <RouterProvider router={router} />
-    </>
+    </AuthProvider>
   );
 }
